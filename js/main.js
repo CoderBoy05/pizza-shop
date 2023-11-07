382b079e6958deb66052afd236902a22ba59db84
argest <aaargest555@gmail.com>
2023-11-07 17:35:41 +0300

    edit select.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,3 @@
+import select from "./select.js";
 
-
-const selectHeader = document.querySelector(".custom-select");
-
-let selectItem = document.querySelectorAll('ustom-select__item');
-
-selectHeader.addEventListener("click", selectToggle);
-
-selectItem.addEventListener("click", selectChoose);
-
-function selectToggle(){
-    selectHeader.classList.toggle("is_active");
-}
-
-function selectChoose(){
-    let text = this.text,
-    currentText = this.closset('.select').querySelector('select__cur')
-    console.log(text);
-}
+select();
---
+++ js/vars.js
@@ -0,0 +1,4 @@
+export const vars = {
+  selectHeader: document.querySelector(".custom-select"),
+  selectItem: document.querySelectorAll(".custom-select__item"),
+};
